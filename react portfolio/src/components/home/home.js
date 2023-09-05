84f3f7d9dbe781dbfc6de8875e4edfe7c8c73edd
Tyler Underwood <twood9600@yahoo.com>
2023-09-05 16:27:56 -0400

    created home js

---
+++ react portfolio/src/components/home/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Style from './Home.module.scss';
+import me from '../../img/self.png';
+import classNames from 'classnames';
+import EmojiBullet from "./EmojiBullet";
+import SocialIcon from "./SocialIcon";
+import {Box} from "@mui/material";
+import {info} from "../../info/Info";
+
